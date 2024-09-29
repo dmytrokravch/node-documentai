f75bbd7fead6ecffee7df695a0439c38f1a89921
Dima <dmytrokravch.art@gmail.com>
2024-09-29 15:59:11 +0200

    feat: add entities extractor

--- index.js
+++ index.js
@@ -18,13 +18,12 @@ const upload = multer({ dest: 'uploads/' });
 
 async function processDocument(filePath) {
   const projectId = 'project-id'; // project ID in Google Cloud Document AI
-  const location = 'pl';
+  const location = 'us';
   const processorId = 'processor-id'; // processor ID in Google Cloud Document AI
 
   const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
   const file = fs.readFileSync(filePath);
   const encodedFile = Buffer.from(file).toString('base64');
-
   const request = {
     name,
     rawDocument: {
@@ -35,23 +34,45 @@ async function processDocument(filePath) {
 
   const [result] = await client.processDocument(request);
   const { document } = result;
-  return document.text;
+  return document;
 }
 
+// function to extract order details from the document ai response
+function extractEntities(document) {
+  const extractedData = {};
+
+  document.entities.forEach(entity => {
+    const type = entity.type; // (np, numerZamowienia)
+    const mentionText = entity.mentionText; // text of the entity
+    const confidence = entity.confidence; // confidence in result
+
+    if (type === 'numerZamowienia') {
+      extractedData.orderNumber = mentionText;
+    }
+
+    if (type === 'rachunekZDnia') {
+      extractedData.orderDate = mentionText;
+    }
+
+    if (type === 'produkt') {
+      extractedData.products = mentionText;
+    }
+  });
+
+  return extractedData;
+}
 
 app.post('/parse-document', upload.single('document'), async (req, res) => {
   try {
     const filePath = req.file.path;
-    const text = await processDocument(filePath);
-    console.log('Parsed text:', text);
+    const processedDocument = await processDocument(filePath);
+
+    const extractedInfo = extractEntities(processedDocument);
+
     // remove the file after processing
     fs.unlinkSync(filePath);
 
-    // Возвращаем данные клиенту
-    res.json({
-      status: 'success',
-      text,
-    });
+    res.json(extractedInfo);
   } catch (error) {
     console.error('Error processing document:', error);
     res.status(500).json({ error: 'Failed to process document' });
